42741163162dfde410bec08e94df86f503c65823
NarathipDev <narathip_aot@hotmail.com>
2023-09-19 16:13:09 +0700

    Add a template of Tic Tac Toe Game.

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tictactoe.board.GameScreen
+import com.example.tictactoe.ui.theme.TicTacToeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TicTacToeTheme {
+                GameScreen(viewModel())
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GamePreview() {
+    TicTacToeTheme {
+        GameScreen(viewModel())
+    }
+}
+
